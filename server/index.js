ba77ab378aabaaa50424573fa901e84c99c5f0e1
Josh Kassman <jkassman2@gmail.com>
2018-07-10 22:22:38 -0400

    Added ability to post in new data to server

--- server/index.js
+++ server/index.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const sqlite3 = require('sqlite3').verbose();
-const bodyParser = require('body-parser');
 
 const app = express();
-app.use(bodyParser);
+app.use(express.json());
 let db = new sqlite3.Database('./db/main.db');
 
 app.get('/api/data', (req, res) => {
     db.all('SELECT * FROM main', (err, rows) => {
         res.json(rows);
     });
+});
+
+app.post('/api/data', (req, res) => {
+    console.log(req.body);
+    let MILES = req.body.MILES;
+    let GAS = req.body.GAS;
+    let PRICE_PER_GAL = req.body.PRICE_PER_GAL;
+    if (typeof MILES !== "number" || typeof GAS !== "number" || typeof PRICE_PER_GAL !== "number") {
+        res.sendStatus(400);
+    }
+    else {
+        db.run("INSERT INTO main VALUES(?, ?, ?)", MILES, GAS, PRICE_PER_GAL, (err) => {
+            if (err) {
+                console.log(err);
+                res.sendStatus(500);
+            }
+            else {
+                res.sendStatus(200);
+            }
+        });
+    }
 });
 
 
