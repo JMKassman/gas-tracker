799402f2acc07fe0bcc066b9a83964d2cc96489a
Josh Kassman <jkassman2@gmail.com>
2018-07-15 20:48:24 -0400

    Add input form to client

--- client/src/App.js
+++ client/src/App.js
@@ -11,14 +11,14 @@ class App extends Component {
     }
 
     componentDidMount() {
-        this.callApi('/api/data')
+        this.getFromApi('/api/data')
             .then(res => this.setState({
                 response: res[0].MILES,
             }))
             .catch(err => console.log(err));
     }
 
-    callApi = async (endpoint) => {
+    getFromApi = async (endpoint) => {
         const response = await fetch(endpoint);
         const body = await response.json();
 
@@ -34,8 +34,90 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Gas Tracker</h1>
                 </header>
+                <InputForm />
             </div>
         );
+    }
+
+
+}
+
+class InputForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            MILES: "0",
+            GAS: "0",
+            PRICE_PER_GAL: "0"
+        };
+
+        this.handleChange = this.handleChange.bind(this);
+        this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    }
+    render() {
+        return (
+            <div className="InputForm">
+                <form onSubmit={this.handleFormSubmit}>
+                    <label>
+                        Miles:
+                        <br />
+                        <input type="text" name="miles" value={this.state.MILES} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Gallons of Gas:
+                        <br />
+                        <input type="text" name="gas" value={this.state.GAS} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <label>
+                        Price per Gallon:
+                        <br />
+                        <input type="text" name="price_per_gal" value={this.state.PRICE_PER_GAL} onChange={this.handleChange} />
+                    </label>
+                    <br />
+                    <br />
+                    <input type="submit" value="Submit" />
+                </form>
+            </div>
+        )
+    }
+
+    handleFormSubmit(event) {
+        let data = {
+            MILES: parseFloat(this.state.MILES),
+            GAS: parseFloat(this.state.GAS),
+            PRICE_PER_GAL: parseFloat(this.state.PRICE_PER_GAL)
+        };
+        if (isNaN(data.MILES) || isNaN(data.GAS) || isNaN(data.PRICE_PER_GAL)) {
+            alert("All inputs must be valid numbers")
+        }
+        else {
+            this.postToAPI(data);
+        }
+        event.preventDefault();
+    }
+
+    handleChange(event) {
+        const state = Object.assign({}, this.state);
+        switch (event.target.name) {
+            case "miles":
+                state.MILES = event.target.value;
+                break;
+            case "gas":
+                state.GAS = event.target.value;
+                break;
+            case "price_per_gal":
+                state.PRICE_PER_GAL = event.target.value;
+                break;
+            default:
+                break;
+        }
+        this.setState(state);
+    }
+
+    postToAPI(data) {
+        console.log("postToAPI called with data " + JSON.stringify(data));
     }
 }
 
